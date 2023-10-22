b39cee0b2f8a55d28a7f3a025850c48ed6aa1bf1
Zeeshan Haider Shaheen <60551271+zeeshanhshaheen@users.noreply.github.com>
2023-10-22 19:21:09 +0200

    routing configuration

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,5 @@
 import * as React from 'react';
-import Button from '@mui/material/Button';
-
-export default function ButtonUsage() {
+export default function Home() {
   return (
     <div>
       <h1>Prototype</h1>
